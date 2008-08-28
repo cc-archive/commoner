cac8336b011869155b113690702c03d3e6d25f85
Nathan R. Yergler <nathan@creativecommons.org>
2008-08-28 10:51:53 -0700

    Collapsing all URLs into a single file.

--- commoner/urls.py
+++ commoner/urls.py
@@ -1,5 +1,5 @@
 
-from django.conf.urls.defaults import patterns, include, handler500
+from django.conf.urls.defaults import patterns, include, handler500, url
 from django.conf import settings
 
 from django.contrib import admin
@@ -13,20 +13,33 @@ urlpatterns = patterns(
     (r'^$', 'django.views.generic.simple.direct_to_template',
      {'template':'index.html'}),
     (r'^admin/(.*)', admin.site.root),
-    (r'^accounts/login/$', 'django.contrib.auth.views.login'),
-    (r'^accounts/', include('registration.urls')),
-    (r'^p/', include('commoner.profiles.urls')),
 
-    (r'^c/(?P<content_id>\d+)/$', 
-        'commoner.profiles.views.content_detail'),
-    )
+    # Account management
+    (r'^a/login/$', 'django.contrib.auth.views.login'),
+    (r'^a/logout/$', 'django.contrib.auth.views.logout'),
+    (r'^a/register/complete/$', 
+     'django.views.generic.simple.direct_to_template',
+     {'template':'registration/success.html'}),
+    (r'^a/register/(?P<key>.*)/', 'commoner.registration.views.complete'),
 
-urlpatterns += patterns(
-    'commoner.server.views',
-    (r'^xrds/$', 'idpXrds'),
-    (r'^processTrustResult/$', 'processTrustResult'),
-    # (r'^user/$', 'idPage'),
-    (r'^endpoint/$', 'endpoint'),
+    # Profile management
+    url(r'^p/edit/$', 'commoner.profiles.views.edit_profile', 
+        name='profile_edit'),
+    url(r'^p/create/$', 'commoner.profiles.views.create_profile', 
+        name='profile_create'),
+
+    # Content details
+    #(r'^c/(?P<content_id>\d+)/$', 
+    #    'commoner.profiles.views.content_detail'),
+    
+    # OpenID Support
+    (r'^o/xrds/$', 'commoner.server.views.idpXrds'),
+    (r'^o/processTrustResult/$', 'commoner.server.views.processTrustResult'),
+    (r'^o/endpoint/$', 'commoner.server.views.endpoint'),
+
+    # Profile view
+    (r'^(?P<username>.*)/$', 'commoner.profiles.views.profile_view'),
+
 )
 
 if settings.DEBUG:
