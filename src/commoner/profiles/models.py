c20fd19d10a77cda0c16fd5a4fa329a43933751f
Nathan R. Yergler <nathan@creativecommons.org>
2008-09-03 17:02:10 +0000

    Support generating a completely absolute URL for the profile; added a display_name method to the Profile model.

--- src/commoner/profiles/models.py
+++ src/commoner/profiles/models.py
@@ -1,6 +1,11 @@
+import urlparse
+
 from django.db import models
 from django.db.models import permalink
+from django.core.urlresolvers import reverse
 from django.contrib.auth.models import User
+
+from commoner.util import getBaseURL
 
 class Content(models.Model):
 
@@ -29,7 +34,16 @@ class CommonerProfile(models.Model):
             return u"%s (%s)" % (self.user.username, self.nickname)
         return self.user.username
 
-    @permalink
-    def get_absolute_url(self):
-        return ('commoner.profiles.views.profile_view', (str(self.user),))
+    def display_name(self):
+        return self.nickname or self.user.username
+
+    def get_absolute_url(self, request=None):
+        if request is None:
+            return reverse('profile_view', args=(self.user.username, ) )
+        else:
+            return urlparse.urljoin(
+                getBaseURL(request), 
+                reverse('profile_view', args=(self.user.username, ) )
+                )
+                
 
