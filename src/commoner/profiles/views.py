4702f9facf2ee5bd30eb5fdc7d49e3762cfd54a1
Nathan R. Yergler <nathan@creativecommons.org>
2008-09-11 09:42:48 -0700

    Removing legacy utils module.

--- src/commoner/profiles/views.py
+++ src/commoner/profiles/views.py
@@ -11,7 +11,6 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 
 import forms
-import utils
 
 @login_required
 def edit_or_create(request):
