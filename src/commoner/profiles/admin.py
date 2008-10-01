673f4f142ae423f75220c55e640c37085c1f10a7
Nathan R. Yergler <nathan@creativecommons.org>
2008-10-01 22:58:20 +0000

    Don't offer the date heirarchy; having any null values pisses it off.

--- src/commoner/profiles/admin.py
+++ src/commoner/profiles/admin.py
@@ -3,6 +3,5 @@ from commoner.profiles.models import CommonerProfile
 
 class CommonerProfileAdmin(admin.ModelAdmin):
     list_display = ('user', 'expires')
-    date_hierarchy = 'expires'
 
 admin.site.register(CommonerProfile, CommonerProfileAdmin)
