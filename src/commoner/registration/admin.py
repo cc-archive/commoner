9619ecae61514bf1681425c503c38ccbe17f4b47
Nathan R. Yergler <nathan@creativecommons.org>
2008-10-02 01:53:52 +0000

    Send welcome emails when registrations are added through the web interface.

--- src/commoner/registration/admin.py
+++ src/commoner/registration/admin.py
@@ -5,4 +5,18 @@ class PartialRegistrationAdmin(admin.ModelAdmin):
 
     list_filter = ('complete',)
 
+    fieldsets = (
+        (None, {
+	    'fields':('last_name', 'first_name', 'email', 'complete', 'transaction_id', 'user'),
+	    'description':'Adding a new registration will send a welcome email.'},
+	),
+	)
+
+    def save_model(self, request, obj, form, change):
+        obj.save()
+
+        if not change:
+	    # creating a new instance
+	    PartialRegistration.objects.send_welcome(obj)
+
 admin.site.register(PartialRegistration, PartialRegistrationAdmin)
