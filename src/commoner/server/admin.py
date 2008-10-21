c5c0305ed9dafadc72c7f7f1101c01195fd845ca
Nathan R. Yergler <nathan@creativecommons.org>
2008-10-21 08:35:29 -0700

    Models for always sharing SREG metadata with trusted parties.

--- src/commoner/server/admin.py
+++ src/commoner/server/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from commoner.server.models import TrustedRelyingParty
+from commoner.server.models import TrustedRelyingParty, TrustedMetadata
+
+class TrustedMetadataAdmin(admin.TabularInline):
+    model = TrustedMetadata
 
 class TrustedRelyingPartyAdmin(admin.ModelAdmin):
     list_display = ('user', 'root')
+    inlines = [TrustedMetadataAdmin]
 
 admin.site.register(TrustedRelyingParty,
                     TrustedRelyingPartyAdmin)
