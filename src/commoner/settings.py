fcea6293ae47cf6d82bf85ec1a398829bd3338b2
Nathan R. Yergler <nathan@creativecommons.org>
2008-10-21 09:31:00 -0700

    MySQL really doesn't like dots in database names.

--- src/commoner/settings.py
+++ src/commoner/settings.py
@@ -21,7 +21,7 @@ DATABASE_HOST = ''             # Set to empty string for localhost. Not used wit
 DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
 
 if DEBUG:
-    DATABASE_NAME='commoner.testing'
+    DATABASE_NAME='commoner_testing'
 
 TIME_ZONE = 'America/Chicago'
 
