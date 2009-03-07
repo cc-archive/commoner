9b56b88c5ea07cc48dd77c132d15c857d9d0899d
John Doig <jedoig@john-doigs-macbook.local>
2009-03-07 14:05:11 -0800

    redirect based on user's redirect_setting field

--- src/commoner/middleware/security.py
+++ src/commoner/middleware/security.py
@@ -1,22 +1,25 @@
-__license__ = "Python"
-__copyright__ = "Copyright (C) 2007, Stephen Zabel"
-__author__ = "Stephen Zabel - sjzabel@gmail.com"
-__contributors__ = "Jay Parlar - parlar@gmail.com"
+# Code sampled from Stephen Zabel - sjzabel@gmail.com
 
 from django.conf import settings
-from django.http import HttpResponseRedirect, HttpResponse, HttpResponsePermanentRedirect, get_host
-
-SSL = 'SSL'
+from django.http import HttpResponsePermanentRedirect, get_host
 
 class SSLMiddleware:
     
     def process_view(self, request, view_func, view_args, view_kwargs):
         secure = request.is_secure()
         if not secure:
-            newurl = "https://%s%s" % (get_host(request),request.get_full_path())
-            if settings.DEBUG and request.method == 'POST':
-                raise RuntimeError, \
-                    """Django can't perform a SSL redirect while maintaining POST data.
-                       Please structure your views so that redirects only occur during GETs."""
             
-            return HttpResponsePermanentRedirect(newurl)
+            try:
+                profile = request.user.get_profile()
+            except ObjectDoesNotExist:
+                profile = None
+            
+            if profile.redirect_https:
+                newurl = "https://%s%s" % (get_host(request),request.get_full_path())
+            
+                if settings.DEBUG and request.method == 'POST':
+                    raise RuntimeError, \
+                        """Django can't perform a SSL redirect while maintaining POST data.
+                           Please structure your views so that redirects only occur during GETs."""
+            
+                return HttpResponsePermanentRedirect(newurl)
