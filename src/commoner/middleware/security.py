16a4d18a5198eb35a8e5bf369d0c99175ae7093a
John Doig <jedoig@john-doigs-macbook.local>
2009-03-10 17:11:50 -0400

    Redirects all requests to HTTPS with the exception of the viewing of a profile.  If the profile view has been called, it checks to see if the user has the redirect_https setting enabled and will redirect accordingly

--- src/commoner/middleware/security.py
+++ src/commoner/middleware/security.py
@@ -1,25 +1,26 @@
-# Code sampled from Stephen Zabel - sjzabel@gmail.com
-
 from django.conf import settings
 from django.http import HttpResponsePermanentRedirect, get_host
+from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
+
+from commoner.profiles.views import view as view_profile
 
 class SSLMiddleware:
     
     def process_view(self, request, view_func, view_args, view_kwargs):
-        secure = request.is_secure()
-        if not secure:
+        
+        if not request.is_secure():
             
-            try:
-                profile = request.user.get_profile()
-            except ObjectDoesNotExist:
-                profile = None
+            if view_func is view_profile:
+                user = get_object_or_404(User, username=view_kwargs['username'])
+                try:
+                    profile = user.get_profile()
+                except ObjectDoesNotExist:
+                    profile = models.CommonerProfile(user=user)
+                if not profile.redirect_https:
+                    return
+                    
+            newurl = "https://%s%s" % (get_host(request),request.get_full_path())
             
-            if profile.redirect_https:
-                newurl = "https://%s%s" % (get_host(request),request.get_full_path())
-            
-                if settings.DEBUG and request.method == 'POST':
-                    raise RuntimeError, \
-                        """Django can't perform a SSL redirect while maintaining POST data.
-                           Please structure your views so that redirects only occur during GETs."""
-            
-                return HttpResponsePermanentRedirect(newurl)
+            return HttpResponsePermanentRedirect(newurl)
