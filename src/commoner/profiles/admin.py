f9244c774af14aa2c767909a0cfc90b67f17828b
John Doig <jedoig@ubuntu.creativecommons.org>
2009-09-17 15:24:39 -0700

    Adding gratif field to the admin display

--- src/commoner/profiles/admin.py
+++ src/commoner/profiles/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from commoner.profiles.models import CommonerProfile
 
 class CommonerProfileAdmin(admin.ModelAdmin):
-    list_display = ('user', 'level', 'nickname', 'expires')
+    list_display = ('user', 'level', 'nickname', 'gratis', 'expires')
 
 admin.site.register(CommonerProfile, CommonerProfileAdmin)
