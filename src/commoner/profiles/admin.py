8a09e08bac0ce0944fbe6cfcc7dba74f5a363155
John Doig <jedoig@ubuntu.creativecommons.org>
2009-09-17 15:34:29 -0700

    Whoops, syntax error.

--- src/commoner/profiles/admin.py
+++ src/commoner/profiles/admin.py
@@ -3,7 +3,7 @@ from commoner.profiles.models import CommonerProfile
 
 def make_gratis(modeladmin, request, queryset):
     queryset.update(gratis=True)
-make_published.short_description = "Mark selected profiles as gratis"
+make_gratis.short_description = "Mark selected profiles as gratis"
 
 class CommonerProfileAdmin(admin.ModelAdmin):
     list_display = ('user', 'level', 'nickname', 'gratis', 'expires')
