f658321469cd6551a10cd02e270eb71512e350ae
John Doig <jedoig@ubuntu.creativecommons.org>
2009-09-17 15:33:10 -0700

    Adding an admin action for batch updating gratis accounts

--- src/commoner/profiles/admin.py
+++ src/commoner/profiles/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from commoner.profiles.models import CommonerProfile
 
+def make_gratis(modeladmin, request, queryset):
+    queryset.update(gratis=True)
+make_published.short_description = "Mark selected profiles as gratis"
+
 class CommonerProfileAdmin(admin.ModelAdmin):
     list_display = ('user', 'level', 'nickname', 'gratis', 'expires')
+    actions = [make_gratis]
 
 admin.site.register(CommonerProfile, CommonerProfileAdmin)
