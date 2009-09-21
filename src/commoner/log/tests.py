fdfc282101a8004e575a6fb1b0557d97e65d0a27
John Doig <jedoig@john-doigs-macbook.local>
2009-09-21 13:50:08 -0700

    More test cases for the Log app

--- src/commoner/log/tests.py
+++ src/commoner/log/tests.py
@@ -1,9 +1,12 @@
-import django.test 
+import django.test
+from django.core import mail
 
 from django.contrib import auth
 from commoner.log.models import LogEntry
 
 class TestLogEntries(django.test.TestCase):
+
+    fixtures = ['test_users.json',]
 
     def test_add_basic_log_entry(self):
 
@@ -14,4 +17,21 @@ class TestLogEntries(django.test.TestCase):
         self.assertEquals(entry.message, "testing message")
         self.assert_(entry.created is not None)
 
-        
+    def test_add_sends_email(self):
+
+        entry = LogEntry("testing", "testing message", send_email=True)
+        entry.save()
+
+        self.assert_(len(mail.outbox) > 0)
+
+    def test_add_entry_with_generic_object(self):
+
+        user = auth.models.User.objects.get(username='testing')
+
+        entry = LogEntry("testing", "testing message", user)
+        entry.save()
+
+        self.assertEquals(entry.message_id, "testing")
+        self.assertEquals(entry.message, "testing message")
+        self.assert_(entry.created is not None)
+        self.assertEquals(entry.content_object, user)
