983e076e340b7dc6fe9a952b03221c00f185c6f0
Nathan R. Yergler <nathan@yergler.net>
2009-09-22 10:18:06 -0700

    Better searching, filter in profile admin.

--- src/commoner/profiles/admin.py
+++ src/commoner/profiles/admin.py
@@ -7,7 +7,10 @@ make_gratis.short_description = "Mark selected profiles as gratis"
 
 class CommonerProfileAdmin(admin.ModelAdmin):
     list_display = ('user', 'level', 'nickname', 'gratis', 'expires')
-    search_fields = ('user__username',)
+    search_fields = ('user__username', 'user__email', 
+                     'user__first_name', 'user__last_name')
+    list_filter = ('gratis', 'level')
+
     actions = [make_gratis]
 
 admin.site.register(CommonerProfile, CommonerProfileAdmin)
