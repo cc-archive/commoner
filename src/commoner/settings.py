144c771351882e7274284441698c6f0e8327b4cd
JED3 <jed@jedpad.(none)>
2010-04-26 13:40:09 -0700

    Goddamnit, stupid error, trim() doesn't exist

--- src/commoner/settings.py
+++ src/commoner/settings.py
@@ -47,7 +47,7 @@ ADMIN_MEDIA_PREFIX = '/admin_media/'
 SECRET_KEY = 'your-secret-key-here'
 # Read the secret key file used for hashing the invitation requests
 INVITE_KEY = open(os.path.join(os.path.dirname(__file__),
-                               'secret-key.txt')).read().trim()
+                               'secret-key.txt')).read().strip()
 # Contribution giving level for invites
 INVITE_AMOUNT = 75.00
 
